mlebihan/openapi-generator

Fix Golang pattern validation with regex fails on commas #20079

modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractGoCodegen.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.*;
+import java.util.regex.Matcher;
 
 import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
@@ -38,6 +39,7 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
 
     private final Logger LOGGER = LoggerFactory.getLogger(AbstractGoCodegen.class);
     private static final String NUMERIC_ENUM_PREFIX = "_";
+    private static final String X_GO_CUSTOM_TAG = "x-go-custom-tag";
 
     @Setter
     protected boolean withGoCodegenComment = false;
@@ -785,9 +787,25 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
                 }
 
                 if (cp.pattern != null) {
-                    cp.vendorExtensions.put("x-go-custom-tag", "validate:\"regexp=" +
-                            cp.pattern.replace("\\", "\\\\").replaceAll("^/|/$", "") +
-                            "\"");
+                    String regexp = String.format(Locale.getDefault(), "regexp=%s", cp.originalPattern);
+
+                    // Replace backtick by \\x60, if found
+                    if (regexp.contains("`")) {
+                        regexp = regexp.replace("`", "\\x60");
+                    }
+
+                    // Escape comma
+                    if (regexp.contains(",")) {
+                        regexp = regexp.replace(",", "\\\\,");
+                    }
+
+                    // as the double quotes will be included in a string, ".......".....", they should be escaped once: ".....\"...."
+                    if (regexp.contains("\"")) {
+                        regexp = regexp.replace("\"", "\\\",");
+                    }
+
+                    String validate = String.format(Locale.getDefault(), "validate:\"%s\"", regexp);
+                    cp.vendorExtensions.put(X_GO_CUSTOM_TAG, validate);
                 }
 
                 // construct data tag in the template: x-go-datatag
@@ -813,8 +831,8 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
                 }
 
                 // {{#vendorExtensions.x-go-custom-tag}} {{{.}}}{{/vendorExtensions.x-go-custom-tag}}
-                if (StringUtils.isNotEmpty(String.valueOf(cp.vendorExtensions.getOrDefault("x-go-custom-tag", "")))) {
-                    goDataTag += " " + cp.vendorExtensions.get("x-go-custom-tag");
+                if (StringUtils.isNotEmpty(String.valueOf(cp.vendorExtensions.getOrDefault(X_GO_CUSTOM_TAG, "")))) {
+                    goDataTag += " " + cp.vendorExtensions.get(X_GO_CUSTOM_TAG);
                 }
 
                 // if it contains backtick, wrap with " instead

jasonxu08/openapi-generator-obsolete

modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/AbstractGoCodegen.java
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.*;
+import java.util.regex.Matcher;
 
 import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
@@ -38,6 +39,7 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
 
     private final Logger LOGGER = LoggerFactory.getLogger(AbstractGoCodegen.class);
     private static final String NUMERIC_ENUM_PREFIX = "_";
+    private static final String X_GO_CUSTOM_TAG = "x-go-custom-tag";
 
     @Setter
     protected boolean withGoCodegenComment = false;
@@ -785,9 +787,20 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
                 }
 
                 if (cp.pattern != null) {
-                    cp.vendorExtensions.put("x-go-custom-tag", "validate:\"regexp=" +
-                            cp.pattern.replace("\\", "\\\\").replaceAll("^/|/$", "") +
-                            "\"");
+                    String regexp = String.format(Locale.getDefault(), "regexp=%s", cp.pattern);
+
+                    // Replace backtick by \\x60, if found
+                    if (regexp.contains("`")) {
+                        regexp = regexp.replace("`", "\\x60");
+                    }
+
+                    // Escape comma
+                    if (regexp.contains(",")) {
+                        regexp = regexp.replace(",", "\\\\,");
+                    }
+
+                    String validate = String.format(Locale.getDefault(), "validate:\"%s\"", regexp);
+                    cp.vendorExtensions.put(X_GO_CUSTOM_TAG, validate);
                 }
 
                 // construct data tag in the template: x-go-datatag
@@ -813,8 +826,8 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
                 }
 
                 // {{#vendorExtensions.x-go-custom-tag}} {{{.}}}{{/vendorExtensions.x-go-custom-tag}}
-                if (StringUtils.isNotEmpty(String.valueOf(cp.vendorExtensions.getOrDefault("x-go-custom-tag", "")))) {
-                    goDataTag += " " + cp.vendorExtensions.get("x-go-custom-tag");
+                if (StringUtils.isNotEmpty(String.valueOf(cp.vendorExtensions.getOrDefault(X_GO_CUSTOM_TAG, "")))) {
+                    goDataTag += " " + cp.vendorExtensions.get(X_GO_CUSTOM_TAG);
                 }
 
                 // if it contains backtick, wrap with " instead
@@ -872,6 +885,11 @@ public abstract class AbstractGoCodegen extends DefaultCodegen implements Codege
         }
 
         return postProcessModelsEnum(objs);
+    }
+
+    @Override
+    public String addRegularExpressionDelimiter(String pattern) {
+        return pattern;
     }
 
     @Override